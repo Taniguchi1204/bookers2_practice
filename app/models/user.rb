b39112c872e51c97c804c6556e6c894348d6c4ac
EC2 Default User <ec2-user@ip-172-31-44-51.ec2.internal>
2021-07-04 22:43:35 +0000

    create relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,24 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   attachment :profile_image, destroy: false
 
+  has_many :followed_users, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :followed_users, source: :following
+
+  has_many :follower_users, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
+  has_many :followings, through: :follower_users, source: :followed
+
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+
+  def follow(user_id)
+    follower_users.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower_users.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only:[:create,:destroy]
+    get "following" => "relationships#following"
+    get "followed" => "relationships#followed"
+  end
   resources :books do
     resource :favorites, only:[:create,:destroy]
     resources :book_comments, only:[:create,:destroy]
